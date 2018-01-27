607daa8d5c95abba18df8592b0968016e4138efe
Billy <williamwidmerr@gmail.com>
2018-01-27 10:10:34 -0800

    detailed comment

--- hotels/quick_search.py
+++ hotels/quick_search.py
@@ -48,4 +48,20 @@ class QuickSearch(object):
             results = value['results']
             merged += results
 
+        '''
+            Just using sorted is pretty fast here - since the results of each
+            API call are sorted themselves.
+            See "Timsort" https://en.wikipedia.org/wiki/Timsort the algorithm
+            python's sorted method uses.
+            "The algorithm finds subsequences of the data that are already ordered,
+            and uses that knowledge to sort the remainder more efficiently."
+            This runs in (n log n). (Its constant time to insert without any rules)
+
+            If we were going to maintain order as we appended to our results list
+            we would have to "sort" as we inserted.
+            An efficient way to do this sounds like some kind of tree, so that our
+            sorted insertion normally takes (log n) time. 
+
+        '''
+
         return sorted(merged, key=lambda x: x['ecstasy'], reverse=True)
